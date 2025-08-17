dd789060a565ba45468550de0701caa0f52985e8
sahad <sahadmuhammed289@gmail.com>
2025-08-18 00:05:30 +0530

    Removed Vite Boilerplate Codes

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+
+// Styles
+import "./index.css";
+
+// Components
+import { App } from "./App";
+
+createRoot(document.getElementById("root")).render(<App />);
